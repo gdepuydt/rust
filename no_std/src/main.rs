fc32d1db875c4e2a6768d6b8827f7e1d95c83378
gdepuydt1983 <geert.depuydt@ontoforce.com>
2019-10-23 11:17:31 +0200

    working in linux... problems in windows?

--- no_std/src/main.rs
+++ no_std/src/main.rs
@@ -1,17 +1,18 @@
 #![feature(lang_items, core_intrinsics)]
 #![feature(start)]
 #![no_std]
+#![no_main]
 use core::intrinsics;
 use core::panic::PanicInfo;
 
 // Pull in the system libc library for what crt0.o likely requires.
 extern crate libc;
 
-// Entry point for this program.
-#[start]
-fn start(_argc: isize, _argv: *const *const u8) -> isize {
-    0
+#[no_mangle] // ensure that this symbol is called `main` in the output
+pub extern fn main(_argc: i32, _argv: *const *const u8) -> i32 {
+    1
 }
+
 
 // These functions are used by the compiler, but not
 // for a bare-bones hello world. These are normally
@@ -29,6 +30,6 @@ pub extern fn rust_eh_unwind_resume() {
 
 #[lang = "panic_impl"]
 #[no_mangle]
-pub extern fn rust_begin_panic(info: &PanicInfo) -> ! {
+pub extern fn rust_begin_panic(_info: &PanicInfo) -> ! {
     unsafe { intrinsics::abort() }
 }
