1ee86672208d8e59b57d1cce384d57aa8e999786
gdepuydt <geert_depuydt@hotmail.com>
2019-10-26 17:51:22 +0200

    little fix

--- print_os/src/main.rs
+++ print_os/src/main.rs
@@ -6,15 +6,17 @@ fn main() {
     
     #[cfg(target_os = "linux")]
     let string = "Hello From Linux!\n";
+    
+    #[cfg(target_os = "windows")]
+    let string = "Hello From Windows!";
+       
     let len = string.len();
     
     unsafe {
+        #[cfg(target_os = "linux")]
         write(0, transmute::<*const u8, *const c_void>(string.as_ptr()), len)
     };
     
-    #[cfg(target_os = "windows")]
-    println!("Hello From Windows!");
-
-    
+       
 
 }
